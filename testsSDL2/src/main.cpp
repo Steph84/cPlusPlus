3179bf2db2e5cdf4c942d48bc9301189bac11c5f
Pirate Captain <st_lerendu@yahoo.fr>
2019-08-21 20:13:31 +0200

    try load image with SDL2_image lib

--- testsSDL2/src/main.cpp
+++ testsSDL2/src/main.cpp
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+#define DATA_PATH "../"
 
 const int WINDOW_WIDTH = 1200;
 const int WINDOW_HEIGHT = 600;
@@ -18,6 +21,8 @@ int main(int argc, char *argv[])
 	SDL_Window* window = nullptr;
 	// SDL_Surface* surface = nullptr; // use RAM
 	SDL_Renderer* renderer = nullptr; // use VRAM
+	SDL_Texture* bg = nullptr;
+	int w, h; // texture width & height
 
 	// create window
 	window = SDL_CreateWindow("SDL2 tests", SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,
@@ -38,6 +43,10 @@ int main(int argc, char *argv[])
 		return -1;	
 	}
 
+	// create background
+	bg = IMG_LoadTexture(renderer, ("%sbackground.png", DATA_PATH));
+	SDL_QueryTexture(bg, NULL, NULL, &w, &h);
+	SDL_Rect texr; texr.x = WINDOW_WIDTH/2; texr.y = WINDOW_HEIGHT/2; texr.w = w*2; texr.h = h*2;
 
 	// game loop
 	while (true)
@@ -52,8 +61,9 @@ int main(int argc, char *argv[])
 			}
 		}
 
-		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+		// SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 		SDL_RenderClear(renderer);
+		SDL_RenderCopy(renderer, bg, NULL, &texr);
 		SDL_RenderPresent(renderer);
 	}
 
